4678e30a2c37b22540739d238bc06bc39ce1d6cc
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-02-26 02:42:55 +0600

    final

--- parallax/settings.py
+++ parallax/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'parallax.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'assignment',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
