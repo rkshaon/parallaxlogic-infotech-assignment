8ba892e74890eaf19d67f348592f87ab07d8f10a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-02-26 02:22:47 +0600

    User profile update with image and other fields

--- user_api/urls.py
+++ user_api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/', views.RegistrationAPIView.as_view(), name='register'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('profile/', views.ProfileAPIView.as_view(), name='profile'),
+    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path('update-profile/', views.UpdateProfileAPIView.as_view(), name='update'),
 ]
--- user_api/views.py
+++ user_api/views.py
@@ -3,10 +3,14 @@ from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import UpdateAPIView
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.parsers import MultiPartParser, FormParser
+
 # from rest_framework.permissions import IsAuthenticated
 
 import jwt
@@ -59,3 +63,49 @@ class ProfileAPIView(APIView):
     def get(self, request):
         serializer = CustomUserSerializer(request.user)
         return Response(serializer.data)
+
+
+class LogoutAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception as e:
+            return Response({"error": "Invalid token", "detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    # def post(self, request):
+    #     # Simply blacklist the refresh token to log out the user
+    #     try:
+    #         refresh_token = request.data["refresh"]
+    #         token = RefreshToken(refresh_token)
+    #         token.blacklist()
+    #         return Response({"message": "User logged out successfully."}, status=200)
+    #     except Exception as e:
+    #         return Response({"error": str(e)}, status=400)
+
+
+class UpdateProfileAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = CustomUserSerializer
+    serializer_class = CustomUserSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    def put(self, request, *args, **kwargs):
+        # your update logic here
+        # make sure to handle the image file properly
+        user = request.user
+        serializer = self.serializer_class(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(status=status.HTTP_200_OK)
